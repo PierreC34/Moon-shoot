5512c891788cf5e1c2ce895eba814ff40072a45f
PierreC34 <62941307+PierreC34@users.noreply.github.com>
2022-03-07 18:34:58 +0100

    Update contact.view.php

--- views/contact.view.php
+++ views/contact.view.php
@@ -59,23 +59,23 @@ ob_start();
             <article class="article-adresse mt-3">
                 <i class="fas fa-map-marker-alt"></i>
                 <p class="p-contact">Adresse</p>
-                <p>2 rue du petit nice</p>
-                <p>34920 Le crès</p>
+                <p>rue</p>
+                <p>code postal</p>
             </article>
             <article class="article-telephone mt-4">
                 <i class="fas fa-phone-alt"></i>
                 <p class="p-contact">Téléphone</p>
-                <p>+33 (0)7 89 87 26 40</p>
+                <p>+33 (0) numéro de telephone</p>
             </article>
             <article class="article-mail mt-4">
                 <i class="fas fa-envelope"></i>
                 <p class="p-contact">Adresse mail</p>
-                <a class="mailto-contact" href="mailto:pierre-frederick.c@hotmail.com">pierre-frederick.c@hotmail.com</a>
+                <a class="mailto-contact" href="mailto:adresse mail">adresse mail</a>
             </article>
         </section>
     </section>
     <article class="article-iframe">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.9355824522977!2d3.9349962157086726!3d43.64950856068705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6a679d0d191df%3A0xbff9797e40387f0e!2s2%20Rue%20du%20Petit%20Nice%2C%2034920%20Le%20Cr%C3%A8s!5e0!3m2!1sfr!2sfr!4v1643119367303!5m2!1sfr!2sfr" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <iframe src="liens de la map google" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </article>
 </div>
 <?php
